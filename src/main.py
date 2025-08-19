e5ea225cd3f87bfb9a9344af315b06e948f70a29
Velotome <noemarzat@gmail.com>
2025-08-19 21:25:27 +0200

    feat : added the storage of measurement and computing of average temp

--- src/main.py
+++ src/main.py
@@ -3,24 +3,18 @@ from _version import __version__
 
 from senseBox import SenseBox
 
-
-app = FastAPI()
-
 print("Starting devops-project v", __version__, " ...", sep="")
 
+app = FastAPI()
 senseBoxes = [
     SenseBox("5eba5fbad46fb8001b799786"),
     SenseBox("5ade1acf223bd80019a1011c"),
     SenseBox("5c21ff8f919bf8001adf2488")
 ]
 
-for box in senseBoxes :
-    box.gather_data()
-
-print(str(senseBoxes[0]))
-print(str(senseBoxes[1]))
-print(str(senseBoxes[2]))
-
-@app.get("/")
-async def root():
-    return {"message" : "Hello World"}
+while True:
+    for sensebox in senseBoxes:
+        sensebox.run()
+    
+    for sensebox in senseBoxes:
+        print(str(sensebox))
--- src/senseBox.py
+++ src/senseBox.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 
 class SenseBox:
     """Class representing a senseBox"""
@@ -7,47 +8,61 @@ class SenseBox:
         self.unique_id = unique_id
         self.name = "N/A"
         self.location = "N/A"
-        self.version = "N/A"
-        self.temperature = "N/A"
-        self.last_mesurement = "N/A"
+        self.average_temp = "N/A"
+        self.max_measurements = 20
+        self.l_measurements = []
 
     def __str__(self):
         ret = f"""
           Sensebox {self.name} :
           - unique_id = {self.unique_id}
           - location = {self.location}
-          - version = {self.version}
-          - temperature = {self.temperature}
-          - last Mesurement = {self.last_mesurement}
-        """
+          - temperature average = {self.average_temp}
+          - number of measurements = {len(self.l_measurements)}
+          - last measurements = {self.l_measurements[len(self.l_measurements)-1]}"""
         return ret
 
+    def run(self):
+        """Run this SenseBox"""
+        self.gather_data()
+        self.compute_average_temp()
+
     def gather_data(self):
-        """Method to gather sensorBox data using an http GET request to opensensmap"""
+        """Method to gather sensorBox data using an http GET request to opensensemap"""
 
+        print(f"{self.unique_id} | Gathering data from opensense.com ... ", end="", flush=True)
         url = f"https://api.opensensemap.org/boxes/{self.unique_id}?format=json"
-        print(f"{self.unique_id} | Gathering data from opensense.com")
-
         data_collected = False
         tries = 0
 
         while not data_collected or tries>3:
             tries += 1
             try:
-                data = requests.get(url, timeout=10).json()
+                data = requests.get(url, timeout=30).json()
                 
                 self.name = data["name"]
                 self.location = data["currentLocation"]["coordinates"]
                 for sensor in data["sensors"]:
                     if sensor["title"] == "Temperatur":
-                        self.temperature = sensor["lastMeasurement"]["value"]
-                        self.last_mesurement = sensor["lastMeasurement"]["createdAt"]
+                        if len(self.l_measurements) >= self.max_measurements:
+                            self.l_measurements.pop(0)
+                        
+                        self.l_measurements.append({
+                            "timestamp": sensor["lastMeasurement"]["createdAt"],
+                            "temperature": float(sensor["lastMeasurement"]["value"])
+                            })
+
 
                 data_collected = True
                 print("Data successfully collected")
             except requests.exceptions.ReadTimeout:
-                print(f"Request to {self.unique_id} has timed out, retrying...")
+                print("Request has timed out, retrying...")
         
         if not data_collected:
-            print(f"Request to {self.unique_id} has timed out too many times, No data gathered")
+            print("Request has timed out too many times, No data gathered")
+    
+    def compute_average_temp(self):
+        """Compute average temp for this senseBox"""
         
+        temps = [d_mesurement["temperature"] for d_mesurement in self.l_measurements]
+        self.average_temp = np.mean(temps)
