8114183ad3e16a0b3caafa867b1a8532225dcfae
Velotome <noemarzat@gmail.com>
2025-08-19 21:25:56 +0200

    prod : modified dockerfile and added test script

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /src
 COPY /src .
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt 
-CMD ["python", "main.py"]
+CMD ["python", "-u", "main.py"]
---
+++ test-docker.sh
@@ -0,0 +1,5 @@
+echo "Removing previous container ..."
+docker rm -f devops-project
+docker build --tag devops-project .
+echo "Running container ..."
+docker run --name=devops-project -d devops-project
